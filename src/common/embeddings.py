23c9eba2a22ab57bfdba1a601ee282fce21a6842
JuNicky <Y.H.Ju@student.tudelft.nl>
2024-05-14 12:09:01 +0200

    refactor and run black on chroma and embeddings

--- src/common/embeddings.py
+++ src/common/embeddings.py
@@ -1,19 +1,35 @@
 from dotenv import load_dotenv
 from langchain_community.embeddings import HuggingFaceEmbeddings
-load_dotenv()
+from langchain_core.embeddings import Embeddings
+from common.huggingface_login import login_huggingface
 
-def getEmbeddings(embeddings_provider, embeddings_model):
-    if embeddings_provider == "huggingface":
-        embeddings = HuggingFaceEmbeddings(model_name=embeddings_model)
-        print(f"[Info] ~ Loaded huggingface embeddings: {embeddings_model}")
-    elif embeddings_provider == "local_embeddings":
-        model_name = embeddings_model
-        model_kwargs = {'device': 'cpu'}
-        encode_kwargs = {'normalize_embeddings': False}
+
+def getEmbeddings(embeddings_provider: str, embeddings_model_name: str) -> Embeddings:
+    """
+    Retrieves embeddings based on the specified provider and model name.
+
+    Args:
+        embeddings_provider (str): The provider of the embeddings. Must be one of ["local_embeddings"].
+        embeddings_model_name (str): The name of the embeddings model.
+
+    Returns:
+        Embeddings: The retrieved embeddings.
+    """
+    if embeddings_provider == "local_embeddings":
+        login_huggingface()
+        model_kwargs = {"device": "cpu"}
+        encode_kwargs = {"normalize_embeddings": False}
         embeddings = HuggingFaceEmbeddings(
-            model_name=model_name,
+            model_name=embeddings_model_name,
             model_kwargs=model_kwargs,
-            encode_kwargs=encode_kwargs
+            encode_kwargs=encode_kwargs,
         )
-        print(f"[Info] ~ Loaded local embeddings: {embeddings_model}")
+
+        if embeddings_model_name == "meta-llama/Meta-Llama-3-8B-Instruct":
+            # Llama3 needs a padding token for the tokenizer.
+            # More info: https://www.reddit.com/r/LangChain/comments/16m1nee/comment/kxw4bfb/?utm_source=share&utm_medium=web3x&utm_name=web3xcss&utm_term=1&utm_content=share_button
+            tokenizer = embeddings.client.tokenizer
+            tokenizer.pad_token = tokenizer.eos_token
+
+        print(f"[Info] ~ Loaded local embeddings: {embeddings_model_name}")
     return embeddings
